85daf4953c60fd72337dd685eaf8f14bf81f7485
Anmol Bhardwaj <anmolbhardwaj18@Anmols-MacBook-Pro.local>
2019-11-05 12:30:44 +0530

    Assignment - I

--- src/demoNode.js
+++ src/demoNode.js
@@ -1,1 +1,16 @@
 console.log("Hello Node");
+
+print= function(msg){
+    console.log(msg);
+}
+secondCounter = function(src,count){
+    for (var i = 0; i < count; i++) {
+        const now = new Date().getTime();
+         while ( now != now + 1000 )
+         {
+            
+        }
+        
+        print(src +": "+i +" seconds are over.");
+    }
+}
